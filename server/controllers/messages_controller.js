86827059038f8b45687bfd4695c6f994d6d46652
maynmike7 <maynmike7@gmail.com>
2020-07-20 16:04:19 -0600

    finished

---
+++ server/controllers/messages_controller.js
@@ -0,0 +1,32 @@
+let messages = [];
+let id = 0;
+
+module.exports = {
+    create: (req, res) => {
+        const {text, time} = req.body;
+        messages.push({id, text, time});
+        id++;
+        res.status(200).send(messages);
+    },
+    read: (req, res) => {
+        res.status(200).send(messages);
+    },
+    update: (req, res) => {
+        const {text} = req.body;
+        const messageIndex = messages.findIndex(message => message.id === +req.params.id)
+        let message = messages[messageIndex];
+
+        messages[messageIndex] = {
+            id: message.id,
+            text: text || message.text,
+            time: message.time
+        }
+
+        res.status(200).send(messages);
+    },
+    delete: (req, res) => {
+        const messageIndex = messages.findIndex(message => message.id === +req.params.id)
+        messages.splice(messageIndex, 1);
+        res.status(200).send(messages);
+    }
+}
